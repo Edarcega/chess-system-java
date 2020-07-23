1512871897a52c4b2d49ec83dc1e1d66f9d2e7f0
Edarcega <edimar.eds@gmail.com>
2020-07-23 12:13:53 -0300

    Little improvement in board printing

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -5,11 +5,9 @@ import chess.ChessMatch;
 public class Programa {
 
 	public static void main(String[] args) {
-		
+
 		ChessMatch chessMatch = new ChessMatch();
-		
+
 		UI.printBoard(chessMatch.getPieces());
-
 	}
-
 }
