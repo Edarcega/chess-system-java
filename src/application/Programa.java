79943eb6cc161c03b5a83967ece838aa2e4ed0a6
Edarcega <edimar.eds@gmail.com>
2020-07-27 21:42:45 -0300

    Knight

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -18,7 +18,7 @@ public class Programa {
 
 		List<ChessPiece> captured = new ArrayList<>();
 
-		while (!chessMatch.getCheck()) {
+		while (!chessMatch.getCheckMate()) {
 
 			try {
 				UI.clearScreen();
